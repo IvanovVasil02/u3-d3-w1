6d23c24f73ccc8ab089f20e8e22c4bc170ea367f
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-09-06 17:21:35 +0200

    add update

--- src/components/SearchBook.jsx
+++ src/components/SearchBook.jsx
@@ -31,7 +31,7 @@ render(){
                </Row>
                <Row className="row-cols-4 px-3 border-1 border-black gy-2">
                     {
-                    this.props.books.filter((book) => book.title.includes(this.state.searchValue)).map((book, index) => (
+                    this.props.books.filter((book) => book.title.toLowerCase().includes(this.state.searchValue)).map((book, index) => (
                         <Col key={`book-${index}`}>
                             <Book src={book.img} title={book.title}  />
                         </Col>
